8dadfd6602f976e7f9a72d6dae3ae78699b106d4
FaiNasserAlkahtani <mariamkalenizi@gmail.com>
2023-10-01 19:40:14 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Mariam's Bakery</h1>
+        <h6>Welcome to My Bakery</h6>
+        <img src="https://www.mashed.com/img/gallery/the-best-bakery-in-every-state/intro-1601499029.jpg"></img>
       </header>
     </div>
   );
